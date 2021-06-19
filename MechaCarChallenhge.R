a610de1986716fde411676aa71f33f1c49c4778f
ManeeshaMovva <74915619+ManeeshaMovva@users.noreply.github.com>
2021-06-19 12:22:02 -0400

    Update MechaCarChallenhge.R

--- MechaCarChallenhge.R
+++ MechaCarChallenhge.R
@@ -8,7 +8,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_car))
 
-#Deliverable 2
+# Deliverable 2
 
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
@@ -16,7 +16,7 @@ total_summary<- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI)
 
 lot_summary<- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance= var(PSI), SD= sd(PSI), .groups="keep")
 
-#Deliverable 3
+# Deliverable 3
 
 t.test(Suspension_Coil$PSI, mu=1500)
 
